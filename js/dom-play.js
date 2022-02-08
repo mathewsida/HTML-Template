608cc7dbe8056fdb546bfbbec1f8a02566f29666
Mathew Sida <sida.mathew@gmail.com>
2022-02-07 18:40:09 -0800

    done

---
+++ js/dom-play.js
@@ -0,0 +1,23 @@
+const spans = document.querySelectorAll('div#play span');
+
+console.log(spans);
+
+for(const mySpan of spans){
+  mySpan.addEventListener('click', function(ev){
+   // alert(mySpan.dataset.actor)
+   highlightActor(mySpan.dataset.actor)
+  });
+}
+
+function highlightActor(myActor){
+
+for(const mySpan of spans){
+
+  if(myActor == mySpan.dataset.actor){
+    mySpan.style.backgroundColor='orange';
+  }else{
+    mySpan.style.backgroundColor='white';
+  }
+
+}
+}
